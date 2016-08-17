143c05e02b867c2942cfc1f2b4e264687311ba4a
Carlos Galvez <carlos.galvez@volvocars.com>
2016-08-17 11:15:09 +0200

    Solve Day 0

---
+++ tutorials/30DaysOfCode/day0.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // Read input
+    std::string text;
+    std::getline(std::cin, text);
+
+    // Print output
+    std::cout << "Hello, World." << std::endl << text;
+    return 0;
+}
